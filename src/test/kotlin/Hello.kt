8edda1d922fe86dd55364b898693ea8ce9f5ff64
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-29 19:57:47 +0000

    ...

--- src/test/kotlin/Hello.kt
+++ src/test/kotlin/Hello.kt
@@ -12,16 +12,27 @@ class HelloTests {
         assert(blocks[0] == listOf(0, 1))
         assert(blocks[1] == emptyList<Int>())
     }
+
     @Test fun `move 1 onto 1`() {
         val blocks = Blocks().move(from = 0, to = 0)
         assert(blocks[0] == listOf(0))
         assert(blocks[1] == listOf(1))
     }
+
+    @Test fun `3 blocks - move 1 onto 1`() {
+        val blocks = Blocks(3).move(from = 0, to = 0)
+        assert(blocks[0] == listOf(0))
+        assert(blocks[1] == listOf(1))
+        assert(blocks[2] == listOf(2))
+    }
 }
 
-class Blocks {
+class Blocks(
+    private val size: Int = 2,
+    private val state: List<List<Int>> = List(size) { listOf(it) }
+) {
     fun move(from: Int, to: Int): List<List<Int>> {
-        if (from == to) return listOf(listOf(0), listOf(1))
+        if (from == to) return state
         if (from > to) return listOf(listOf(0, 1), emptyList())
         return listOf(emptyList(), listOf(1, 0))
     }
