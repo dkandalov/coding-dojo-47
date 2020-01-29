253545b088a6d34eedc2cbd24f1c28ab251f9c11
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-29 19:19:25 +0000

    ...

--- src/test/kotlin/Hello.kt
+++ src/test/kotlin/Hello.kt
@@ -1,8 +1,19 @@
+
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
 
 class HelloTests {
-    @Test fun foo() {
-        fail("aaa")
+    @Test fun `minimal move of two blocks`() {
+        val blocks = Block().move(from = 1, to = 2)
+        assert(blocks[0] == emptyList<Int>())
+        assert(blocks[1] == listOf(1, 0))
     }
+}
+
+class Block {
+   fun move(from: Int, to: Int): List<List<Int>> {
+       return listOf(
+           emptyList(),
+           listOf(1, 0)
+       )
+   }
 }
