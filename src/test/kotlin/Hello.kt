4e7416fb3751adf589067e0357f3c2b5ca85b004
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-29 20:03:50 +0000

    ...

--- src/test/kotlin/Hello.kt
+++ src/test/kotlin/Hello.kt
@@ -5,18 +5,21 @@ class HelloTests {
         val blocks = Blocks().move(from = 0, to = 1)
         assert(blocks[0] == emptyList<Int>())
         assert(blocks[1] == listOf(1, 0))
+        assert(blocks.size == 2)
     }
 
     @Test fun `2 blocks - move 2 onto 1`() {
         val blocks = Blocks().move(from = 1, to = 0)
         assert(blocks[0] == listOf(0, 1))
         assert(blocks[1] == emptyList<Int>())
+        assert(blocks.size == 2)
     }
 
     @Test fun `2 blocks - move 1 onto 1`() {
         val blocks = Blocks().move(from = 0, to = 0)
         assert(blocks[0] == listOf(0))
         assert(blocks[1] == listOf(1))
+        assert(blocks.size == 2)
     }
 
     @Test fun `3 blocks - move 1 onto 1`() {
@@ -24,13 +27,7 @@ class HelloTests {
         assert(blocks[0] == listOf(0))
         assert(blocks[1] == listOf(1))
         assert(blocks[2] == listOf(2))
-    }
-
-    @Test fun `3 blocks - move 1 onto 2`() {
-        val blocks = Blocks(3).move(from = 0, to = 1)
-        assert(blocks[0] == emptyList<Int>())
-        assert(blocks[1] == listOf(1, 0))
-        assert(blocks[2] == listOf(2))
+        assert(blocks.size == 3)
     }
 }
 
@@ -41,6 +38,6 @@ class Blocks(
     fun move(from: Int, to: Int): List<List<Int>> {
         if (from == to) return state
         if (from > to) return listOf(listOf(0, 1), emptyList())
-        return listOf(emptyList(), listOf(1, 0), listOf(2))
+        return listOf(emptyList(), listOf(1, 0))
     }
 }
