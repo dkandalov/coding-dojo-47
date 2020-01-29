d1843184fa095daa4d5603d7ac80856baa754875
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-29 19:50:27 +0000

    ...

--- src/test/kotlin/Hello.kt
+++ src/test/kotlin/Hello.kt
@@ -1,25 +1,28 @@
-import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 class HelloTests {
-    @Test fun `minimal move of two blocks`() {
-        val blocks = Block().move(from = 1, to = 2)!!
+    @Test fun `move 1 onto 2`() {
+        val blocks = Blocks().move(from = 0, to = 1)
         assert(blocks[0] == emptyList<Int>())
         assert(blocks[1] == listOf(1, 0))
     }
 
-    @Test fun `minimal illegal move of two blocks`() {
-        val blocks = Block().move(from = 2, to = 1)
-        assertNull(blocks)
+    @Test fun `move 2 onto 1`() {
+        val blocks = Blocks().move(from = 1, to = 0)
+        assert(blocks[0] == listOf(0, 1))
+        assert(blocks[1] == emptyList<Int>())
+    }
+    @Test fun `move 1 onto 1`() {
+        val blocks = Blocks().move(from = 0, to = 0)
+        assert(blocks[0] == listOf(0))
+        assert(blocks[1] == listOf(1))
     }
 }
 
-class Block {
-    fun move(from: Int, to: Int): List<List<Int>>? {
-        if (from > to) return null
-        return listOf(
-            emptyList(),
-            listOf(1, 0)
-        )
+class Blocks {
+    fun move(from: Int, to: Int): List<List<Int>> {
+        if (from == to) return listOf(listOf(0), listOf(1))
+        if (from > to) return listOf(listOf(0, 1), emptyList())
+        return listOf(emptyList(), listOf(1, 0))
     }
 }
